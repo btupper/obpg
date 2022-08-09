0d5ffcee218358267d06e809d54179a8e0cc1101
Johnathan Evanilla <jevanilla@bigelow.org>
2022-08-09 17:37:25 -0400

    Updated obpg_url to work with modis aqua and terra

--- R/obpg.R
+++ R/obpg.R
@@ -23,14 +23,32 @@ obpg_url <- function(date = Sys.Date() - 2,
   
   if (inherits(date, "character")) date <- as.Date(date)  
   
-  src <- sprintf("%s_%s", toupper(mission[1]), toupper(instrument[1]))
+  #src <- sprintf("%s_%s", toupper(mission[1]), toupper(instrument[1]))
+  src <- switch(instrument,
+                "AQUA" = "AQUA_MODIS",
+                "TERRA" = "TERRA_MODIS",
+                "VIIRS" = "SNPP_VIIRS")
   
   product <- sprintf("L3m.%s.%s", period, product)
+  
+  root_mission <- switch(instrument,
+                         "AQUA" = "https://oceandata.sci.gsfc.nasa.gov/opendap/MODISA",
+                         "TERRA" = "https://oceandata.sci.gsfc.nasa.gov/opendap/MODIST",
+                         "VIIRS" = "https://oceandata.sci.gsfc.nasa.gov/opendap/VIIRS")
   
   name <- sprintf("%s.%s.%s.%s.nc",
                   src,
                   format(date, "%Y%m%d"),
                   product,
                   resolution)
-  file.path(root, instrument, level, format(date, "%Y"), format(date, "%m%d"), name)                     
+  file.path(root_mission, level, format(date, "%Y"), format(date, "%m%d"), name)                     
 }
+
+
+# http://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/2021/0713/AQUA_MODIS.20210713.L3m.DAY.CHL.chlor_a.9km.nc
+
+#"https://oceandata.sci.gsfc.nasa.gov/opendap/MODISA/L3SMI/2021/0713/AQUA_MODIS.20210713.L3m.DAY.CHL.chlor_a.9km.nc"
+
+
+# https://oceandata.sci.gsfc.nasa.gov/opendap/VIIRS/L3SMI/2022/0806/SNPP_VIIRS.20220806.L3m.DAY.SST.sst.9km.nc
+
